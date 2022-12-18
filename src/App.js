be79c8f4ed0e66a1b8aa835428885aaeeb58f0df
khoatran5773 <khoa.tran4@tiki.vn>
2022-12-18 12:18:46 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import Board from './Board';
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className='App-h1'><span className='App-orange'>LIGHTS</span>  <span className="App-blue">OUT</span></h1>
-      <Board size={5} chanceLightStartsOn={0.25} />
+    return (
+        <div className="App">
+            <h1 className='App-h1'><span className='App-orange'>LIGHTS</span>  <span className="App-blue">OUT</span></h1>
+            <Board size={5} chanceLightStartsOn={0.25} />
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
--- src/Cell.js
+++ src/Cell.js
@@ -1,17 +1,17 @@
 import React from "react";
 import './Cell.css';
 
-function Cell(props){
+function Cell(props) {
     const { cellIndex, isOn, toggleLight } = props;
 
     function handleToggleLight() {
         toggleLight(cellIndex);
-        
+
     }
 
     return (
-        <button 
-            className={isOn?"Cell-on":"Cell-off"} 
+        <button
+            className={isOn ? "Cell-on" : "Cell-off"}
             onClick={handleToggleLight}
         ></button>
     );
